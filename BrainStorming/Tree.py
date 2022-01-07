97119286a1a712820f3d0c3108857175e0ab0e3a
rakib3004 <bsse1129@iit.du.ac.bd>
2022-01-08 00:05:16 +0600

    add roots pre in and post order traversal

--- BrainStorming/Tree.py
+++ BrainStorming/Tree.py
@@ -4,12 +4,40 @@ class TreeNode:
         self.left = None
         self.right = None
 
-def add_left_child(self, node):
-    self.left = node
+    def add_left_child(self, node):
+        self.left = node
 
-def add_right_child(self, node):
-    self.right = node
+    def add_right_child(self, node):
+        self.right = node
 
+
+
+
+
+
+
+
+
+def inorder_traversal(node):
+    if node.left:
+        inorder_traversal(node.left)
+    print(node.value, end=" -> ")
+    if node.right:
+        inorder_traversal(node.right)
+
+def postorder_traversal(node):
+    if node.left:
+        preorder_traversal(node.left)
+    if node.right:
+        preorder_traversal(node.right)
+    print(node.value, end=" -> ")
+
+def preorder_traversal(node):
+    print(node.value, end=" -> ")
+    if node.left:
+        postorder_traversal(node.left)
+    if node.right:
+        postorder_traversal(node.right)
 
 
 if __name__ == "__main__":
@@ -20,6 +48,16 @@ if __name__ == "__main__":
     root.add_right_child(three)
     two.add_left_child(four)
     three.add_left_child(five)
-    three.add_rigth_child(six)
+    three.add_right_child(six)
 
+    print("----------Inorder Traversal----------")
     inorder_traversal(root)
+    print()
+
+    print("----------Preorder Traversal----------")
+    preorder_traversal(root)
+    print()
+
+    print("----------Postorder Traversal----------")
+    postorder_traversal(root)
+    print()
