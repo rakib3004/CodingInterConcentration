855dd57689e589eab49cf48796b600e2575d63a7
rakib3004 <bsse1129@iit.du.ac.bd>
2022-02-15 18:41:10 +0600

    Add and write Youtube video to Audio

---
+++ PyTube/VideoToAudio.py
@@ -0,0 +1,19 @@
+from pytube import YouTube
+
+# where to save.
+# replce /home/balasundar/Downloads/ with the path where you want to store the dowload file
+destination = "C:\\Users\\pc\\Downloads"
+# link of the video to be downloaded
+# Replace with the Youtube video link you want to download.
+video_link = "https://www.youtube.com/watch?v=xWOoBJUqlbI"
+
+try:
+    video = YouTube(video_link)
+    # filtering the audio. File extension can be mp4/webm
+    # You can see all the available streams by print(video.streams)
+    audio = video.streams.filter(only_audio=True, file_extension='mp4').first()
+    audio.download()
+    print('Download Completed!')
+
+except:
+    print("Connection Error")  # to handle exception
